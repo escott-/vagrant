3605d99f58a6dd7648ad39254b1012da4bd31473
Erik Scott <escott05@gmail.com>
2017-02-16 10:21:44 -0800

    add java, elasticsearch, couchbase

--- provision.sh
+++ provision.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo -i
 
 # vagrant init bento/ubuntu-16.04; vagrant up --provider virtualbox
 ADDR=192.168.55.55
@@ -24,6 +25,14 @@ ntp \
 inotify-tools \
 tcl8.5
 
+# Install Java
+echo "BEGIN JAVA INSTALL"
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+apt-get install oracle-java8-installer -y
+
 # Install Redis
 echo "BEGIN REDIS INSTALL"
 wget http://download.redis.io/releases/redis-stable.tar.gz
@@ -45,6 +54,22 @@ apt-get install couchdb -y
 COUCHDB_CONF="/etc/couchdb/local.ini"
 sed -i "s/\(;bind_address \).*/\bind_address = $ADDR /" $COUCHDB_CONF
 service couchdb restart
+
+# Install Couchbase
+echo "BEGIN COUCHBASE INSTALL"
+wget https://packages.couchbase.com/releases/4.5.0/couchbase-server-community_4.5.0-ubuntu14.04_amd64.deb
+dpkg-deb -x couchbase-server-community_4.5.0-ubuntu14.04_amd64.deb $HOME
+cd $HOME/opt/couchbase
+./bin/install/reloc.sh `pwd`
+./bin/couchbase-server -- -noinput -detached
+
+# Install Elasticsearch
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.1.deb
+dpkg -i elasticsearch-5.2.1.deb
+ELASTIC_CONF="/etc/elasticsearch/elasticsearch.yml"
+sed -i "s/\(#network.host: \).*/network.host: $ADDR /" $ELASTIC_CONF
+sed -i "s/\(#http.port: \).*/http.port: 9200 /" $ELASTIC_CONF
+service elasticsearch start
 
 # Postgres
 echo "BEGIN POSTGRES INSTALL"
